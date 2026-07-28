f5504e20da547ae0fc1689fecbc1fcee0310ba4d
LucSousa21 <souza.lucas1805@gmail.com>
2026-07-27 22:44:15 -0300

    refactor: refatora código e adiciona comentários explicativos

--- app/schemas/cliente.py
+++ app/schemas/cliente.py
@@ -1,19 +1,35 @@
 from pydantic import BaseModel, EmailStr, field_validator
-from datetime import datetime
 import phonenumbers
 
-class Cliente(BaseModel):
-    nome_cliente: str
-    telefone_cliente: str
-    email_cliente: EmailStr
-    @field_validator("telefone_cliente")
+
+# Classe responsável pelos dados de entrada do cadastro.
+class CadastrarCliente(BaseModel):
+    nome: str
+    telefone: str
+    email: EmailStr
+
+    @field_validator("telefone")
     @classmethod
     def validar_telefone(cls, value):
         try:
             numero = phonenumbers.parse(value, "BR")
+
             if not phonenumbers.is_valid_number(numero):
                 raise ValueError("Telefone inválido")
+
         except:
             raise ValueError("Telefone inválido")
 
         return value
+
+
+
+# Classe responsável pelo formato da resposta da API.
+class ClienteResponse(BaseModel):
+    cliente_id: int
+    nome: str
+    telefone: str
+    email: EmailStr
+
+    class Config:
+        from_attributes = True
